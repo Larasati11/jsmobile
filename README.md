7c41b849f1518f55f009d45e26790ba364f384cb
Larasati11 <larasati.puspita1101@gmail.com>
2024-10-13 12:47:04 +0700

    Done : add Pertemuan 6 Basic Layout

---
+++ README.md
@@ -0,0 +1,36 @@
+# Laporan Praktikum Layout dan Navigasi
+Nama    : Larasati Puspita Candra Dewi
+NIM     : 2241720104
+Kelas   : TI-2B
+Absen   : 16
+
+## Tugas 1 Bagian Codelab
+### Create a visible widget
+![alt text](image.png)
+
+### Material Apps
+![alt text](image-1.png)
+
+### Cupertino Apps
+![alt text](image-2.png)
+
+### Non-Material Apps
+![alt text](image-3.png)
+
+### Aligning Widget
+Row :
+![alt text](image-4.png)
+
+Column :
+![alt text](image-5.png)
+
+### Sizing Widget
+![alt text](image-6.png)
+
+![alt text](image-7.png)
+
+### Packing Widget
+![alt text](image-8.png)
+
+### Card 
+![alt text](image-9.png)
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( // Pastikan MaterialApp ada di sini
+      home: Scaffold( // Scaffold memberikan struktur dasar untuk aplikasi
+        appBar: AppBar(
+          title: const Text('Card Example'), // Menambahkan judul aplikasi
+        ),
+        backgroundColor: Colors.white, // Latar belakang putih
+        body: Center(
+          child: _buildCard(), // Memanggil fungsi _buildCard
+        ),
+      ),
+    );
+  }
+
+  // Fungsi untuk membangun card
+  Widget _buildCard() {
+    return SizedBox(
+      height: 210,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text(
+                '1625 Main Street',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              subtitle: const Text('My City, CA 99984'),
+              leading: Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              title: const Text(
+                '(408) 555-1212',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            ListTile(
+              title: const Text('costa@example.com'),
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
